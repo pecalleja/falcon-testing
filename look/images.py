2832c2583a9714dc4945ccfeb299f6e9e020e715
Pedro Enrique Calleja Acosta <pecalleja@gmail.com>
2018-08-29 17:56:47 -0500

    Error handling

--- look/images.py
+++ look/images.py
@@ -6,6 +6,18 @@ import re
 import uuid
 import mimetypes
 
+ALLOWED_IMAGE_TYPES = (
+    'image/gif',
+    'image/jpeg',
+    'image/png',
+)
+
+
+def validate_image_type(req, resp, resource, params):
+    if req.content_type not in ALLOWED_IMAGE_TYPES:
+        msg = 'Image type not allowed. Must be PNG, JPEG, or GIF'
+        raise falcon.HTTPBadRequest('Bad request', msg)
+
 
 class Collection(object):
 
@@ -13,6 +25,7 @@ class Collection(object):
     def __init__(self, image_store):
         self._image_store = image_store
 
+    @falcon.before(validate_image_type)
     def on_post(self, req, resp):
         name = self._image_store.save(req.stream, req.content_type)
         resp.status = falcon.HTTP_201
@@ -43,7 +56,10 @@ class Item(object):
 
     def on_get(self, req, resp, name):
         resp.content_type = mimetypes.guess_type(name)[0]
-        resp.stream, resp.stream_len = self._image_store.open(name)
+        try:
+            resp.stream, resp.stream_len = self._image_store.open(name)
+        except IOError:
+            raise falcon.HTTPNotFound()
 
 
 class ImageStore(object):
